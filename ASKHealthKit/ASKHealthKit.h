52e80efb788bbe4ab165e4f9e7506a1ea7461e93
sato-shin <shin.sato@w-it.co.jp>
2018-07-13 11:51:33 +0900

    Initial Commit

---
+++ ASKHealthKit/ASKHealthKit.h
@@ -0,0 +1,19 @@
+//
+//  ASKHealthKit.h
+//  ASKHealthKit
+//
+//  Created by sato-shin on 2018/07/13.
+//  Copyright © 2018年 com.askendiet. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ASKHealthKit.
+FOUNDATION_EXPORT double ASKHealthKitVersionNumber;
+
+//! Project version string for ASKHealthKit.
+FOUNDATION_EXPORT const unsigned char ASKHealthKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ASKHealthKit/PublicHeader.h>
+
+
