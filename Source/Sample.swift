cf8b7954d47e6fab0550f45c629c6d04d5bcdf2c
sato-shin <sankaku.button@gmail.com>
2018-09-18 02:38:46 +0900

    wip

---
+++ Source/Sample.swift
@@ -0,0 +1,9 @@
+//
+// Created by sato-shin on 2018/09/15.
+// Copyright (c) 2018 com.askendiet. All rights reserved.
+//
+
+import Foundation
+
+class Sample {
+}
---
+++ Source/hoge.swift
@@ -0,0 +1,9 @@
+//
+//  hoge.swift
+//  ASKHealthKit
+//
+//  Created by sato-shin on 2018/09/17.
+//  Copyright © 2018年 com.askendiet. All rights reserved.
+//
+
+import Foundation
